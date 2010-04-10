01d3e8c787b2e47d51e6de71c2497c5d2de4b2ee
makoto kuwata <kwa@kuwata-lab.com>
2010-04-10 23:00:30 +0900

    python: add 'MANIFEST{,.in}', 'setup.py{,.txt}'

---
+++ python/setup.py
@@ -0,0 +1,66 @@
+###
+### $Release: 0.2.0 $
+### $Copyright: copyright(c) 2010 kuwata-lab.com all rights reserved $
+### $License: MIT License $
+###
+
+import sys, re, os
+arg1 = len(sys.argv) > 1 and sys.argv[1] or None
+if arg1 == 'egg_info':
+    from ez_setup import use_setuptools
+    use_setuptools()
+if arg1 == 'bdist_egg':
+    from setuptools import setup
+else:
+    from distutils.core import setup
+
+
+name     = 'Oktest'
+version  = '0.2.0'
+author   = 'makoto kuwata'
+email    = 'kwa@kuwata-lab.com'
+maintainer = author
+maintainer_email = email
+url      = 'http://www.kuwata-lab.com/oktest/'
+desc     = 'a new-style testing library'
+detail   = (
+           'Oktest is a new-style testing library.\n'
+           )
+license  = 'MIT License'
+platforms = 'any'
+download = 'http://downloads.sourceforge.net/oktest/%s-%s.tar.gz' % (name, version)
+#download = 'http://jaist.dl.sourceforge.net/sourceforge/tenjin/%s-%s.tar.gz' % (name, version)
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.4',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.0',
+    'Programming Language :: Python :: 3.1',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+]
+
+
+setup(
+    name=name,
+    version=version,
+    author=author,  author_email=email,
+    maintainer=maintainer, maintainer_email=maintainer_email,
+    description=desc,  long_description=detail,
+    url=url,  download_url=download,  classifiers=classifiers,
+    license=license,
+    #platforms=platforms,
+    #
+    py_modules=['oktest'],
+    package_dir={'': 'lib'},
+    #scripts=['bin/pytenjin'],
+    #packages=['tenjin'],
+    zip_safe = False,
+)
