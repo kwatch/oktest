979ee3b66be9648d357c52f3983408fba3af7db1
makoto kuwata <kwa@kuwata-lab.com>
2010-05-16 16:21:23 +0900

    python: update 'setup.py' to support Python 2.7

--- python/setup.py
+++ python/setup.py
@@ -62,6 +62,7 @@ classifiers = [
     'Programming Language :: Python :: 2.4',
     'Programming Language :: Python :: 2.5',
     'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.0',
     'Programming Language :: Python :: 3.1',
