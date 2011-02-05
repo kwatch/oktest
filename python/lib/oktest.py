7315c539f1ab29a947dcfebeb236dc57dac141e2
makoto kuwata <kwa@kuwata-lab.com>
2011-02-05 21:08:38 +0900

    python: release preparation for 0.7.0

--- python/lib/oktest.py
+++ python/lib/oktest.py
@@ -3,7 +3,7 @@
 ###
 ### oktest.py -- new style test utility
 ###
-### $Release: 0.6.0 $
+### $Release: 0.7.0 $
 ### $Copyright: copyright(c) 2010-2011 kuwata-lab.com all rights reserved $
 ### $License: MIT License $
 ###
