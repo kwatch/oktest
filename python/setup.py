efc60d2daceb6a2d10e5788d3a8b42aa7a40be87
makoto kuwata <kwa@kuwata-lab.com>
2011-06-15 13:51:54 +0900

    python: update 'setup.py'

--- python/setup.py
+++ python/setup.py
@@ -15,75 +15,81 @@ else:
     from distutils.core import setup
 
 
-name     = 'Oktest'    ## $Package: Oktest $
-version  = '0.8.0'     ## $Release: 0.8.0 $
-author   = 'makoto kuwata'
-email    = 'kwa@kuwata-lab.com'
-maintainer = author
-maintainer_email = email
-url      = 'http://pypi.python.org/pypi'
-desc     = 'a new-style testing library'
-detail   = (
-            "Oktest is a new-style testing library.\n"
-            "::\n"
-            "\n"
-            "    from oktest import ok, NG\n"
-            "    ok (x) > 0                 # same as assert_(x > 0)\n"
-            "    ok (s) == 'foo'            # same as assertEqual(s, 'foo')\n"
-            "    ok (s) != 'foo'            # same as assertNotEqual(s, 'foo')\n"
-            "    ok (f).raises(ValueError)  # same as assertRaises(ValueError, f)\n"
-            "    ok (u'foo').is_a(unicode)  # same as assert_(isinstance(u'foo', unicode))\n"
-            "    NG (u'foo').is_a(int)  # same as assert_(not isinstance(u'foo', int))\n"
-            "    ok ('A.txt').is_file()     # same as assert_(os.path.isfile('A.txt'))\n"
-            "    NG ('A.txt').is_dir()  # same as assert_(not os.path.isdir('A.txt'))\n"
-            "\n"
-            "You can use ok() instead of 'assertXxx()' in unittest.\n"
-            "\n"
-            "Oktest requires Python 2.4 or later. Oktest is ready for Python 3.\n"
-            "\n"
-            "NOTICE!! Oktest is a young project and specification may change in the future.\n"
-            "\n"
-            "See README_ for details.\n"
-            "\n"
-            ".. _README: http://packages.python.org/Oktest\n"
-           )
-license  = 'MIT License'     ## $License: MIT License $
-platforms = 'any'
-#download = 'http://downloads.sourceforge.net/oktest/Oktest-%s.tar.gz' % version
-download = 'http://pypi.python.org/packages/source/O/Oktest/Oktest-%s.tar.gz' % version
-classifiers = [
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    #'Programming Language :: Python :: 2.3',
-    'Programming Language :: Python :: 2.4',
-    'Programming Language :: Python :: 2.5',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.0',
-    'Programming Language :: Python :: 3.1',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Topic :: Software Development :: Testing'
-]
+def fn():
+    name       = 'Oktest'    ## $Package: Oktest $
+    version    = '0.8.0'     ## $Release: 0.8.0 $
+    author     = 'makoto kuwata'
+    email      = 'kwa@kuwata-lab.com'
+    maintainer = author
+    maintainer_email = email
+    url        = 'http://pypi.python.org/pypi'
+    desc       = 'a new-style testing library'
+    detail     = r"""
+Oktest is a new-style testing library for Python. ::
+
+    from oktest import test, ok, NG
+
+    class FooTest(unittest.TestCase):
+
+       @test("1 + 1 should be 2")
+       def _(self):
+          ok (1+1) == 2          # same as assertEqual(2, 1+1)
+
+       @test("other examples")
+       def _(self):
+          ok (s) == 'foo'        # same as assertEqual(s, 'foo')
+          ok (s) != 'foo'        # same as assertNotEqual(s, 'foo')
+          ok (n) > 0             # same as assert_(n > 0)
+          ok (fn).raises(Error)  # same as assertRaises(Error, fn)
+          ok ([]).is_a(list)     # same as assert_(isinstance([], list))
+          NG ([]).is_a(tuple)    # same as assert_(not isinstance([], tuple))
+          ok ('A.txt').is_file() # same as assert_(os.path.isfile('A.txt'))
+          NG ('A.txt').is_dir()  # same as assert_(not os.path.isdir('A.txt'))
+
+Features:
+
+* ``ok()`` is provided which is much shorter than ``self.assertXxxx()``.
+* Allow to write test name in free text.
+* Fixture injection support.
+* Tracer class is provided which can be used as mock or stub.
+* Text diff (diff -u) is displayed when texts are different.
+
+Oktest requires Python 2.4 or later (3.x is supported).
+
+See README_ for details.
+
+.. _README: http://packages.python.org/Oktest
+"""[1:]
+    license    = 'MIT License'     ## $License: MIT License $
+    platforms  = 'any'
+    #download  = 'http://downloads.sourceforge.net/oktest/Oktest-%s.tar.gz' % version
+    download   = 'http://pypi.python.org/packages/source/O/Oktest/Oktest-%s.tar.gz' % version
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        #'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing'
+    ]
+
+    py_modules   = ['oktest']
+    package_dir  = {'': 'lib'}
+    #scripts     = ['bin/pytenjin']
+    #packages    = ['tenjin']
+    zip_safe     = False
+
+    return locals()
 
 
-setup(
-    name=name,
-    version=version,
-    author=author,  author_email=email,
-    maintainer=maintainer, maintainer_email=maintainer_email,
-    description=desc,  long_description=detail,
-    url=url,  download_url=download,  classifiers=classifiers,
-    license=license,
-    #platforms=platforms,
-    #
-    py_modules=['oktest'],
-    package_dir={'': 'lib'},
-    #scripts=['bin/pytenjin'],
-    #packages=['tenjin'],
-    zip_safe = False,
-)
+setup(**fn())
