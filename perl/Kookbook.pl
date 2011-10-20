5602dd38da38a1c5828208f9a7060c867d93c062
makoto kuwata <kwa@kuwata-lab.com>
2011-10-21 02:05:57 +0900

    perl: add 'Kookbook.pl'

---
+++ perl/Kookbook.pl
@@ -0,0 +1,17 @@
+
+$kook_default = 'test';
+
+recipe 'test', {
+    desc   => 'do test',
+    method => sub {
+        sys 'prove t';
+        #sys 'bin/oktest.pl examples';
+    }
+};
+
+recipe 'examples', {
+    desc   => 'run examples',
+    method => sub {
+        sys 'bin/oktest.pl examples';
+    }
+};
