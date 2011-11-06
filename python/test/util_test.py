75ff990dedc400eb5c0357c395c96b2ed396fc72
makoto kuwata <kwa@kuwata-lab.com>
2011-11-06 17:45:27 +0900

    python: refactor 'test/util_test.py'

--- python/test/util_test.py
+++ python/test/util_test.py
@@ -11,8 +11,7 @@ import unittest
 import oktest
 from oktest import ok, NG, run, spec
 from oktest import python2, python3
-from oktest.util import *
-from oktest.util import flatten, rm_rf
+from oktest.util import chdir, flatten, rm_rf
 
 
 available_with_statement = sys.version_info[0:2] > (2, 4)
@@ -24,7 +23,7 @@ class DummyClass(object):
     pass
 
 
-def do_exec(script, **kwargs):
+def _do_exec(script, **kwargs):
     if not available_with_statement:
         print("*** skipped")
         return
@@ -39,205 +38,217 @@ def do_exec(script, **kwargs):
 
 class _Context_TC(unittest.TestCase):
 
-    def setUp(self):
-        self.ctx = oktest._Context()
 
     def test___enter__(self):
-        if spec("returns self."):
-            ok (self.ctx.__enter__()).is_(self.ctx)
+        """returns self."""
+        ctx = oktest._Context()
+        ok (ctx.__enter__()).is_(ctx)
+
 
     def test__exit__(self):
-        if spec("returns None."):
-            ok (self.ctx.__exit__()) == None
+        """returns None."""
+        ctx = oktest._Context()
+        ok (ctx.__exit__()) == None
+
 
 
 class _RunnableContext_TC(unittest.TestCase):
 
-    def test_run(self):
+
+    def test_run_1(self):
+        """calls __enter__() and __exit__() to emurate with-statement."""
         ctx = oktest._RunnableContext()
         ret = None
+        called = []
+        def enter(*args):
+            called.append(('enter', args))
+        def exit(*args):
+            called.append(('exit', args))
         #
-        if spec("calls __enter__() and __exit__() to emurate with-statement."):
-            called = []
-            def enter(*args):
-                called.append(('enter', args))
-            def exit(*args):
-                called.append(('exit', args))
-            #
-            ctx.__enter__ = enter
-            ctx.__exit__  = exit
-            def func(*args, **kwargs):
-                called.append(('func', args, kwargs))
-                return 123
-            #
-            ret = ctx.run(func, 'a', b=1)
-            ok (called[0]) == ('enter', ())
-            ok (called[1]) == ('func', ('a',), {'b': 1})
-            ok (called[2]) == ('exit', (None, None, None))
+        ctx.__enter__ = enter
+        ctx.__exit__  = exit
+        def func(*args, **kwargs):
+            called.append(('func', args, kwargs))
+            return 123
         #
-        if spec("returns value which func returned."):
-            ok (ret) == 123
+        ret = ctx.run(func, 'a', b=1)
+        ok (called[0]) == ('enter', ())
+        ok (called[1]) == ('func', ('a',), {'b': 1})
+        ok (called[2]) == ('exit', (None, None, None))
 
-    def test_deco(self):
+    def test_run_2(self):
+        """returns value which func returned."""
         ctx = oktest._RunnableContext()
-        if spec("decorates function."):
-            @ctx.deco
-            def func(x):
-                return x + 10
-            ret = func(123)
-            ok (ret) == 133
-        if spec("__enter__() and __exit__() are called when decoreated function called."):
-            called = []
-            def enter(*args): called.append(('enter', args))
-            def exit(*args): called.append(('exit', args))
-            ctx.__enter__ = enter
-            ctx.__exit__ = exit
-            @ctx.deco
-            def func(*args):
-                called.append(('func', args))
-                return 999
-            ok (called) == []
-            ret = func(456)
-            ok (called[0]) == ('enter', ())
-            ok (called[1]) == ('func', (456,))
-            ok (called[2]) == ('exit', (None, None, None))
-            ok (ret) == 999
+        def func():
+            return 123
+        ret = ctx.run(func)
+        ok (ret) == 123
+
+
+    def test_deco_1(self):
+        """decorates function."""
+        ctx = oktest._RunnableContext()
+        @ctx.deco
+        def func(x):
+            return x + 10
+        ret = func(123)
+        ok (ret) == 133
+
+    def test_deco_2(self):
+        """__enter__() and __exit__() are called when decoreated function called."""
+        ctx = oktest._RunnableContext()
+        called = []
+        def enter(*args): called.append(('enter', args))
+        def exit(*args): called.append(('exit', args))
+        ctx.__enter__ = enter
+        ctx.__exit__ = exit
+        @ctx.deco
+        def func(*args):
+            called.append(('func', args))
+            return 999
+        ok (called) == []
+        ret = func(456)
+        ok (called[0]) == ('enter', ())
+        ok (called[1]) == ('func', (456,))
+        ok (called[2]) == ('exit', (None, None, None))
+        ok (ret) == 999
+
 
 
 class Spec_TC(unittest.TestCase):
 
-    def test___init__(self):
-        if spec("takes description."):
-            obj = oktest.Spec('foobar')
-            ok (obj.desc) == 'foobar'
 
-    def test___iter__(sefl):
-        if spec("emurates with-stmt when used with for-stmt."):
-            called = []
-            def enter(*args):
-                called.append(('enter', args))
-            def exit(*args):
-                called.append(('exit', args))
-            obj = oktest.Spec('foo')
-            obj.__enter__ = enter
-            obj.__exit__  = exit
-            i = 0
-            for x in obj:
-                i += 1
-                called.append(('yield', x))
-                ok (x).is_(obj)
-            ok (i) == 1
-            ok (called[0]) == ('enter', ())
-            ok (called[1]) == ('yield', obj)
-            ok (called[2]) == ('exit', (None, None, None))
+    def test___init___1(self):
+        """takes description."""
+        obj = oktest.Spec('foobar')
+        ok (obj.desc) == 'foobar'
 
-    def test___bool__(self):
+
+    def test___iter___1(sefl):
+        """emurates with-stmt when used with for-stmt."""
+        called = []
+        def enter(*args):
+            called.append(('enter', args))
+        def exit(*args):
+            called.append(('exit', args))
+        obj = oktest.Spec('foo')
+        obj.__enter__ = enter
+        obj.__exit__  = exit
+        i = 0
+        for x in obj:
+            i += 1
+            called.append(('yield', x))
+            ok (x).is_(obj)
+        ok (i) == 1
+        ok (called[0]) == ('enter', ())
+        ok (called[1]) == ('yield', obj)
+        ok (called[2]) == ('exit', (None, None, None))
+
+
+    def test___bool___1(self):
+        """returns True when $SPEC is not set."""
         obj = oktest.Spec('SOS')
-        #
-        if spec("returns True when $SPEC is not set."):
+        ok (bool(obj)) == True
+
+    def test___bool___2(self):
+        """returns False when $SPEC not matched to SPEC."""
+        obj = oktest.Spec('SOS')
+        try:
+            os.environ['SPEC'] = 'SOS'
             ok (bool(obj)) == True
-        #
-        if spec("returns False when $SPEC not matched to SPEC."):
-            try:
-                os.environ['SPEC'] = 'SOS'
-                ok (bool(obj)) == True
-                os.environ['SPEC'] = 'SOS!'
-                ok (bool(obj)) == False
-                os.environ['SPEC'] = 'OS'
-                ok (bool(obj)) == True
-            finally:
-                os.environ.pop('SPEC')
+            os.environ['SPEC'] = 'SOS!'
+            ok (bool(obj)) == False
+            os.environ['SPEC'] = 'OS'
+            ok (bool(obj)) == True
+        finally:
+            os.environ.pop('SPEC')
+
 
 
 class GLOBAL_TC(unittest.TestCase):
 
-    def test_spec(self):
+
+    def test_spec_1(self):
+        """returns oktest.Spec object."""
         obj = oktest.spec('Haruhi')
-        if spec("returns oktest.Spec object."):
-            ok (obj).is_a(oktest.Spec)
-        if spec("takes description."):
-            ok (obj.desc) == 'Haruhi'
+        ok (obj).is_a(oktest.Spec)
+
+    def test_spec_2(self):
+        """takes description."""
+        obj = oktest.spec('Haruhi')
+        ok (obj.desc) == 'Haruhi'
+
 
 
 class util_TC(unittest.TestCase):
 
-    def test_chdir(self):
+
+    def _before_chdir(self):
+        self.tmpdir = '_tmp_dir'
+        os.mkdir(self.tmpdir)
+        self.cwd = os.getcwd()
+        self.tmpdir_path = os.path.abspath(self.tmpdir)
+
+    def _after_chdir(self):
+        os.rmdir(self.tmpdir)
+
+
+    def test_chdir_1(self):
+        """change directory temporarily when used with with-stmt."""
         try:
-            tmpdir = '_tmp_dir'
-            os.mkdir(tmpdir)
-            cwd = os.getcwd()
-            tmpdir_path = os.path.abspath(tmpdir)
-            #+
-            if spec("change directory temporarily when used with with-stmt."):
-                script = r"""if True:
+            self._before_chdir()
+            tmpdir, cwd, tmpdir_path = self.tmpdir, self.cwd, self.tmpdir_path
+            script = r"""if True:
                 import oktest
                 with oktest.util.chdir(tmpdir):
                     ok (os.getcwd()) != cwd
                     ok (os.getcwd()) == tmpdir_path
                 ok (os.getcwd()) == cwd  """
-                do_exec(script, tmpdir=tmpdir, cwd=cwd, tmpdir_path=tmpdir_path)
-            #-
-            #
-            if spec("change directory temporarily when run() called with function."):
-                def f():
-                    ok (os.getcwd()) != cwd
-                    ok (os.getcwd()) == tmpdir_path
-                oktest.util.chdir(tmpdir, f)
-                ok (os.getcwd()) == cwd
-                oktest.util.chdir(tmpdir).run(f)
-                ok (os.getcwd()) == cwd
+            _do_exec(script, tmpdir=tmpdir, cwd=cwd, tmpdir_path=tmpdir_path)
         finally:
-            os.rmdir(tmpdir)
+            self._after_chdir()
 
-    def test_using(self):
-        #+
-        if spec("takes a class object and adds local vars to it when used with with-stmt."):
-            script = r"""if True:
-            import oktest
-            with oktest.util.using(DummyClass) as obj:
-                def test_1(self):
-                    ok (1+1) == 2
-                def test_2(self):
-                    ok (1-1) == 0  """
-            do_exec(script, DummyClass=DummyClass)
-            if available_with_statement:
-                ok (DummyClass).has_attr('test_1')
-                ok (DummyClass).has_attr('test_2')
-        #-
-        pass
-
-    def test_flatten(self):
-        if spec("flatten nested list or tuple."):
-            ret = oktest.util.flatten([[1, (2, (3, 4)), 5]])
-            ok (ret) == [1, 2, 3, 4, 5]
-            ret = flatten([1, [2, 3, [4, 5, [[[6]]]], [7, 8]]])
-            ok (ret) == [1, 2, 3, 4, 5, 6, 7, 8]
-
-    def test_rm_rf(self):
+    def test_chdir_2(self):
+        """change directory temporarily when run() called with function."""
         try:
-            os.mkdir('_test_foo1')
-            os.mkdir('_test_foo2')
-            os.mkdir('_test_foo2/bar1')
-            for fname in ('_test_foo2/bar1/baz.txt', '_test_bar9'):
-                f = open(fname, 'w')
-                f.write(fname)
-                f.close()
-                ok (fname).is_file()
+            self._before_chdir()
+            tmpdir, cwd, tmpdir_path = self.tmpdir, self.cwd, self.tmpdir_path
             #
-            if spec("removes files and directories recursively."):
-                oktest.util.rm_rf('_test_foo1', '_test_foo2', '_test_bar9')
-                ok ('_test_foo1').not_exist()
-                ok ('_test_foo2').not_exist()
-                ok ('_test_bar9').not_exist()
+            def f():
+                ok (os.getcwd()) != cwd
+                ok (os.getcwd()) == tmpdir_path
+            oktest.util.chdir(tmpdir, f)
+            ok (os.getcwd()) == cwd
+            oktest.util.chdir(tmpdir).run(f)
+            ok (os.getcwd()) == cwd
         finally:
-            for x in ['_test_foo1', '_test_foo2']:
-                if os.path.isdir(x):
-                    shutil.rmtree(x)
+            self._after_chdir()
 
 
-class util_rm_rf_TC(unittest.TestCase):
+    def test_using_1(self):
+        """takes a class object and adds local vars to it when used with with-stmt."""
+        script = r"""if True:
+        import oktest
+        with oktest.util.using(DummyClass) as obj:
+            def test_1(self):
+                ok (1+1) == 2
+            def test_2(self):
+                ok (1-1) == 0  """
+        _do_exec(script, DummyClass=DummyClass)
+        if available_with_statement:
+            ok (DummyClass).has_attr('test_1')
+            ok (DummyClass).has_attr('test_2')
 
-    def setUp(self):
+
+    def test_flatten_1(self):
+        """flatten nested list or tuple."""
+        ret = oktest.util.flatten([[1, (2, (3, 4)), 5]])
+        ok (ret) == [1, 2, 3, 4, 5]
+        ret = flatten([1, [2, 3, [4, 5, [[[6]]]], [7, 8]]])
+        ok (ret) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+    def _before_rm_rf(self):
         os.mkdir('_rm_rf')
         os.mkdir('_rm_rf/A')
         os.mkdir('_rm_rf/B')
@@ -246,30 +257,55 @@ class util_rm_rf_TC(unittest.TestCase):
         f = open('_rm_rf/Y.txt', 'w'); f.write('yyy'); f.close()
         assert os.path.isfile('_rm_rf/B/C/X.txt')
         assert os.path.isfile('_rm_rf/Y.txt')
-    def tearDown(setup):
+
+    def _after_rm_rf(setup):
         if os.path.isdir('_rm_rf'):
             shutil.rmtree('_rm_rf')
-    def test_remove_files_recursively(self):
-        args = ['_rm_rf/A', '_rm_rf/B', '_rm_rf/Y.txt']
-        rm_rf(*args)
-        for arg in flatten(args):
-            ok (os.path.exists(arg)) == False
-    def test_flatten_args(self):
-        args = ['_rm_rf/A', ['_rm_rf/B', '_rm_rf/Y.txt']]
-        rm_rf(args)
-        for arg in flatten(args):
-            ok (os.path.exists(arg)) == False
-    def test_ignore_unexist_files(self):
-        args = ['_rm_rf/A', '_rm_rf/K', '_rm_rf/Z.txt']
-        def f():
+
+    def test_rm_rf_1(self):
+        """remove files or directories recursively"""
+        self._before_rm_rf()
+        try:
+            rm_rf('_rm_rf')
+            ok ('_rm_rf').not_exist()
+        finally:
+            self._after_rm_rf()
+
+    def test_rm_rf_2(self):
+        """accepts arguments"""
+        self._before_rm_rf()
+        try:
+            args = ['_rm_rf/A', '_rm_rf/B', '_rm_rf/Y.txt']
             rm_rf(*args)
-        NG (f).raises(Exception)
+            ok ('_rm_rf/A').not_exist()
+            ok ('_rm_rf/B').not_exist()
+            ok ('_rm_rf/Y.txt').not_exist()
+        finally:
+            self._after_rm_rf()
+
+    def test_rm_rf_3(self):
+        """flatten args"""
+        self._before_rm_rf()
+        try:
+            args = ['_rm_rf/A', ['_rm_rf/B', '_rm_rf/Y.txt']]
+            rm_rf(args)
+            for arg in flatten(args):
+                ok (os.path.exists(arg)) == False
+        finally:
+            self._after_rm_rf()
+
+    def test_rm_rf_4(self):
+        """ignore unexist files"""
+        self._before_rm_rf()
+        try:
+            def fn():
+                rm_rf('_rm_rf/A', '_rm_rf/K', '_rm_rf/Z.txt')
+            ok (fn).not_raise()
+        finally:
+            self._after_rm_rf()
 
 
-
-class Util_TC(unittest.TestCase):
-
-    def do_test(self, input):
+    def _do_test(self, input):
         fname = "__test_input.txt"
         if python2:
             assert isinstance(input, str)
@@ -294,30 +330,30 @@ class Util_TC(unittest.TestCase):
             finally:
                 os.path.exists(fname) and os.unlink(fname)
 
-    def test__read_text_file__with_magic_comment(self):
-        ## with magic comment
+    def test_read_text_file_1(self):
+        """with magic comment"""
         input = r"""
 # -*- coding: cp932 -*-
 あいうえお
 """[1:]
-        self.do_test(input)
+        self._do_test(input)
 
-    def test__read_text_file__with_shebang(self):
-        ## with shebang
+    def test_read_text_file_2(self):
+        """with shebang"""
         input = r"""
 #!/usr/bin/env python
 # -*- coding: cp932 -*-
 あいうえお
 """[1:]
-        self.do_test(input)
+        self._do_test(input)
 
-    def test__read_text_file__when_no_magic_comment(self):
-        ## without magic comment
+    def test_read_text_file_3(self):
+        """without magic comment"""
         input = r"""
 あいうえお
 """[1:]
         try:
-            self.do_test(input)
+            self._do_test(input)
             self.fail('UnicodeDecodeError expected but not raised')
         except Exception:
             ex = sys.exc_info()[1]
