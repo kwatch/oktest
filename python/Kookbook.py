6afd1824c8d9bf76873423bf3663b7564a886de7
makoto kuwata <kwa@kuwata-lab.com>
2012-08-04 14:03:28 +0900

    python: release preparation for 0.11.1

--- python/Kookbook.py
+++ python/Kookbook.py
@@ -20,7 +20,7 @@ from glob import glob
 from kook.utils import read_file, write_file
 
 package   = prop('package', 'Oktest')
-release   = prop('release', '0.11.0')
+release   = prop('release', '0.11.1')
 copyright = prop('copyright', "copyright(c) 2010-2011 kuwata-lab.com all rights reserved")
 license   = "MIT License"
 python    = prop('python', sys.executable)
