0054b0a893da17c4e1b653166c01f87032b07f0a
makoto kuwata <kwa@kuwata-lab.com>
2013-03-08 20:36:14 +0900

    python: update 'mainapp_test.py' for py3.0 and 3.1

--- python/test/mainapp_test.py
+++ python/test/mainapp_test.py
@@ -296,9 +296,9 @@ ERROR: never done
 Traceback (most recent call last):
   File "/usr/local/lib/python/unittest.py", line 254, in run
     testMethod()
-  File "/usr/local/lib/python/site-packages/oktest.py", line 1868, in newfunc
+  File "/usr/local/lib/python/site-packages/oktest.py", line 1894, in newfunc
     return orig_func(self)
-  File "/usr/local/lib/python/site-packages/oktest.py", line 536, in fn
+  File "/usr/local/lib/python/site-packages/oktest.py", line 562, in fn
     raise SkipTest(reason)
 oktest.SkipTest: REASON
 
@@ -306,11 +306,11 @@ oktest.SkipTest: REASON
 ERROR: not yet
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File "/usr/local/lib/python/site-packages/oktest.py", line 557, in deco
+  File "/usr/local/lib/python/site-packages/oktest.py", line 583, in deco
     func(*args, **kwargs)
   File "_test.d/_sos_test.py", line 28, in _
     fail("msg")
-  File "/usr/local/lib/python/site-packages/oktest.py", line 470, in fail
+  File "/usr/local/lib/python/site-packages/oktest.py", line 496, in fail
     raise AssertionError(desc)
 AssertionError: msg
 
@@ -319,9 +319,9 @@ During handling of the above exception, another exception occurred:
 Traceback (most recent call last):
   File "/usr/local/lib/python/unittest.py", line 254, in run
     testMethod()
-  File "/usr/local/lib/python/site-packages/oktest.py", line 1868, in newfunc
+  File "/usr/local/lib/python/site-packages/oktest.py", line 1894, in newfunc
     return orig_func(self)
-  File "/usr/local/lib/python/site-packages/oktest.py", line 560, in deco
+  File "/usr/local/lib/python/site-packages/oktest.py", line 586, in deco
     raise _ExpectedFailure(sys.exc_info())
 oktest._ExpectedFailure: expected failure
 
@@ -370,11 +370,11 @@ ERROR: test_005: not yet (_sos_test.SosTest)
 not yet
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File "/usr/local/lib/python/site-packages/oktest.py", line 557, in deco
+  File "/usr/local/lib/python/site-packages/oktest.py", line 583, in deco
     func(*args, **kwargs)
   File "_test.d/_sos_test.py", line 28, in _
     fail("msg")
-  File "/usr/local/lib/python/site-packages/oktest.py", line 470, in fail
+  File "/usr/local/lib/python/site-packages/oktest.py", line 496, in fail
     raise AssertionError(desc)
 AssertionError: msg
 
@@ -383,9 +383,9 @@ During handling of the above exception, another exception occurred:
 Traceback (most recent call last):
   File "/usr/local/lib/python/unittest.py", line 480, in run
     testMethod()
-  File "/usr/local/lib/python/site-packages/oktest.py", line 1868, in newfunc
+  File "/usr/local/lib/python/site-packages/oktest.py", line 1894, in newfunc
     return orig_func(self)
-  File "/usr/local/lib/python/site-packages/oktest.py", line 560, in deco
+  File "/usr/local/lib/python/site-packages/oktest.py", line 586, in deco
     raise _ExpectedFailure(sys.exc_info())
 oktest._ExpectedFailure: expected failure
 
