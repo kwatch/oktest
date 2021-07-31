b7eaae39a0d9cc1beaff23048dbd2556204fba2f
kwatch <kwatch@gmail.com>
2021-07-31 21:47:43 +0900

    ruby: define 'test:all' task

--- ruby/Rakefile.rb
+++ ruby/Rakefile.rb
@@ -11,6 +11,24 @@ task :test do
   ruby "test/run_all.rb"
 end
 
+if ENV['VS_HOME']
+  desc "do test for different ruby versions"
+  task :'test:all' do
+    ruby_versions = %w[2.4 2.5 2.6 2.7 3.0]
+    vs_home = ENV['VS_HOME'].split(/:/).first
+    ENV['TC_QUIET'] = "Y"
+    comp = proc {|x, y| x.to_s.split('.').map(&:to_i) <=> y.to_s.split('.').map(&:to_i) }
+    ruby_versions.each do |ver|
+      dir = Dir.glob("#{vs_home}/ruby/#{ver}.*").sort_by(&comp).last
+      next unless dir
+      puts "==== ruby #{ver} (#{dir}) ===="
+      sh "#{dir}/bin/ruby test/run_all.rb" do |ok, res|
+        $stderr.puts "** test failed" unless ok
+      end
+    end
+  end
+end
+
 desc "retrieve scripts from README.txt"
 task :retrieve do
   mkdir 'tmp' unless File.directory?('tmp')
