5a7889d97fe90e19e1ba9a11890e91f4f908b07d
kwatch <kwatch@gmail.com>
2021-08-02 14:48:07 +0900

    ruby: enhance 'FixtureManager' to support global scope

--- ruby/lib/oktest.rb
+++ ruby/lib/oktest.rb
@@ -914,6 +914,8 @@ module Oktest
         return val
       elsif topic.parent
         return get_fixture_value(name, topic.parent, spec, context, location, resolved, resolving)
+      elsif ! topic.eql?(GLOBAL_SCOPE)
+        return get_fixture_value(name, GLOBAL_SCOPE, spec, context, location, resolved, resolving)
       else
         ex = FixtureNotFoundError.new("#{name}: fixture not found. (spec: #{spec.desc})")
         ex.set_backtrace([location])
