380efa7778de76b70dfe841ff890ac38954a0a50
kwatch <kwatch@gmail.com>
2024-10-07 07:19:35 +0900

    ruby: define 'DEFAULT_REPORTING_STYLE' instead of 'REPORTER_CLASS'

--- ruby/lib/oktest.rb
+++ ruby/lib/oktest.rb
@@ -2168,9 +2168,6 @@ END
   end
 
 
-  REPORTER_CLASS = VerboseReporter
-
-
   REPORTER_CLASSES = {
     'verbose' => VerboseReporter,  'v' => VerboseReporter,
     'simple'  => SimpleReporter,   's' => SimpleReporter,
@@ -2179,10 +2176,13 @@ END
     'quiet'   => QuietReporter,    'q' => QuietReporter,
   }
 
+  DEFAULT_REPORTING_STYLE = 'verbose'
+
 
   def self.run(reporter: nil, style: nil)
     #; [!6xn3t] creates reporter object according to 'style:' keyword arg.
-    klass = (style ? REPORTER_CLASSES[style] : REPORTER_CLASS)  or
+    style ||= DEFAULT_REPORTING_STYLE
+    klass = REPORTER_CLASSES[style]  or
       raise ArgumentError, "#{style.inspect}: unknown style."
     reporter ||= klass.new
     #; [!mn451] run test cases.
